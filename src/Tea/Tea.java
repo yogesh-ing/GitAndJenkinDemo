3133a9e7bd37ad1bc828466d58bc72d15d94fcb3
yogesh-ing <36414126+yogesh-ing@users.noreply.github.com>
2019-09-15 20:29:56 +0530

    Update Tea.java

--- src/Tea/Tea.java
+++ src/Tea/Tea.java
@@ -3,13 +3,13 @@ package Tea;
 public class Tea {
 	
 	static int teaMake=0;
-	/*Tea(){
+	Tea(){
 		Milk milk = new Milk();
 		Water water = new Water();
 		TeaLeaf teaLeaf = new TeaLeaf();
 		Sugar sugar = new Sugar();
 		teaMake = milk.quantity+water.quantity+teaLeaf.quantity+sugar.quantity;
-	}*/
+	}
 	
 	public static void main(String[] args) {
 		
